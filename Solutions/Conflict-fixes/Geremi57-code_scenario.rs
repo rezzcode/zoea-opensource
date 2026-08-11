af7aecada38fe3fe974873353f62bffb8bc5ea02
Geeezo-rama <169067053+Geremi57@users.noreply.github.com>
2026-08-11 14:19:23 +0300

    Introduce conflicting change for Geremi57 (version 3) (#42)
    Co-authored-by: github-actions <154701770+rezzcode@users.noreply.github.com>

--- Solutions/Conflict-fixes/Geremi57-code_scenario.rs
+++ Solutions/Conflict-fixes/Geremi57-code_scenario.rs
@@ -1,12 +1,12 @@
 /**
- * A program that takes a value, eg b, and returns its type
+ * A program that takes a value, eg 5, and returns its type
  * plus other types
  * 
  * Note: the program doesn't accept strings
  * 
  * How to use: 
- * rezzcode@linux$ rustc code_scenario.rs
- * rezzcode@linux$ ./code_scenario.rs 8
+ * rezzcode@linux$ rustc scenario.rs
+ * rezzcode@linux$ ./scenario.rs 2
  * 
  * 4 is of type i32: 4 bytes
  * Other types:
@@ -57,7 +57,7 @@ fn main() {
     }
 }
 
-// Print sizes of types excluding the one that matched
+// Print sizes of types, excluding the one that matched
 fn print_other_types(exclude: &str) {
     println!("Other types:");
     if exclude != "char" {
@@ -67,7 +67,7 @@ fn print_other_types(exclude: &str) {
         println!("Size of i32: {} bytes", mem::size_of::<i32>());
     }
     if exclude != "u32" {
-        println!("Size of u32: {} bytes", mem::size_of::<u32>());
+        println!("Size of i32: {} bytes", mem::size_of::<i32>()); // bug to fix
     }
     if exclude != "f64" {
         println!("Size of f64: {} bytes", mem::size_of::<f64>());
